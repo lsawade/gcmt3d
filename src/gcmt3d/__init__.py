4d4c452e5fab100c03009b10b35dee552b19fd99
Lucas Sawade <lsawade@princeton.edu>
2019-02-27 15:33:54 -0500

    The downloader class is buildable, but needs some testing as well as it is missing a lot of functionality

--- src/gcmt3d/__init__.py
+++ src/gcmt3d/__init__.py
@@ -1,5 +1,41 @@
 """Package __init__.py docstring
 Let's see whether Sphinx will print this Package Docstring"""
 
+# Declare namespace
+__import__('pkg_resources').declare_namespace(__name__)
+
 # Import packages:
 from .source import CMTSource
+
+
+# The function below I found on stackoverflow. It is used to import  all
+# subpackages and modules, when gcmt3d is imported without specification of the
+# subpackages
+
+import importlib
+import pkgutil
+import sys
+
+def import_submodules(package_name):
+    """ Import all submodules of a module, recursively
+
+    :param package_name: Package name
+    :type package_name: str
+    :rtype: dict[types.ModuleType]\
+
+
+    From Stackoverflow: User:   kolypto
+                        Posted: 01.08.2014
+                        Found:
+
+    Added by Lucas Sawade 27.02.2019
+
+
+    """
+    package = sys.modules[package_name]
+    return {
+        name: importlib.import_module(package_name + '.' + name)
+        for loader, name, is_pkg in pkgutil.walk_packages(package.__path__)
+    }
+
+import_submodules(__name__)
--- src/gcmt3d/data_download/requestBuilder.py
+++ src/gcmt3d/data_download/requestBuilder.py
@@ -24,7 +24,8 @@ class InputError(Error):
     def __init__(self, expression, message):
         self.expression = expression
         self.message = message
-
+        print(self.expression + "\n\n")
+        print(self.message + "\n")
 
 
 class dataRequest(object):
@@ -34,25 +35,111 @@ class dataRequest(object):
 
     """
 
-    def __init__(self,cmt=None,stationlist=None,duration=0.0):
+    def __init__(self, cmt=None, stationlist=None, channels=[], locations=[],
+                        duration=0.0):
         """
-
         :param cmt: CMTSource object from cmt Source
-        :param stationlist:
-
+        :param stationlist: list of station in format
+                            `[['Net1','Sta1'],['Net2','Sta2'],etc.]`
+        :param channels: list of strings containing wanted channels
+        :param locations: list of seismometer locations
+        :param duration: duration of the requested recording from origin time
+        :param :
         """
-        if type(cmt)!='CMTSource':
-            raise InputError("No or non-CMTSource chosen as input\n\n",
-                             "Choose a different CMT source.\n\n")
-        self.origin_time = cmt.origin
-        self.stationlist
-        self.duration = duration
+
+        # Check if CMT input is a CMT object
+        if type(cmt) != CMTSource:
+            raise InputError("dataRequest initialization",
+                             "None or incorrect CMT input.")
+
+        # CMT parameter input
+        self.origin_time = cmt.cmt_time
+        self.origin_latitude = cmt.latitude
+        self.origin_longitude = cmt.longitude
+        self.eventname = cmt.eventname
+
+        # Check if station list is a 2D list of networks and stations
+        if type(stationlist) != list:
+            raise InputError("dataRequest initialization",
+                             "stationlist not a list")
+
+        # Station list parameter setup
+        self.stationlist = stationlist
+
+
+        # Download parameters
+        self.duration  = duration
+        self.channels  = channels
+        self.locations = locations
+
+
+    @classmethod
+    def from_file(cls,cmtfname,
+                      stationlistfname,
+                      channels=[],
+                      locations=[],
+                      duration=0.0):
+        """Creates a downloader class from an input file
+
+        This downloader class also needs to contain the ouput directory for the
+        traces. Take CMT fname
+        """
+
+        # First Create CMT Solution
+        cmt = CMTSource.from_CMTSOLUTION_file(cmtfname)
+
+        # Read station list file with two columns and whitespace separation
+        statfile = open(stationlistfname,'r')
+
+        # For loop to add all stations to the station list
+        stationlist = [];
+        for line in statfile:
+            if line=="NETWORK	STATION":
+                continue
+
+            # Read stations into list of stations
+            line = line.split()
+
+            # Append the network o
+            stationlist.append([line[0],line[1]])
+
+        return cls(cmt=cmt,
+                   stationlist=stationlist,
+                   channels=channels,
+                   locations=locations,
+                   duration=duration)
+
+
+    def request(self):
+        """
+        :returns str: with the actual url request
+        """
+        pass
+
+    def download(self,path):
+        """
+        takes the path of the earthquake (from file)
+        """
 
 
     def __str__(self):
         """
-        key download info printing
+        String that contains key download info
         """
-        return_str = 'request build'
+        return_str  = "Earthquake Parameters\n"
+        return_str += "--------------------------------------------------\n"
+        return_str += "Earthquake ID: %s\n" % self.eventname
+        return_str += "Origin Time: %s\n" % self.origin_time
+        return_str += "Origin Latitude: %s\n" % self.origin_latitude
+        return_str += "Origin Longitude: %s\n\n" % self.origin_longitude
+
+        return_str += "Download Parameters\n"
+        return_str += "--------------------------------------------------\n"
+        return_str += "Duration [s]: %s\n" % self.duration
+        return_str += "Channels: %s\n" % self.channels
+        return_str += "Locations: %s\n" % self.origin_latitude
+        return_str += "Origin Longitude: %s\n" % self.origin_longitude
+
 
         return return_str
+
