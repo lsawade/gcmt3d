04a5b238fa45afb351492cd0a1bd9af59a8e70be
Lucas Sawade <lsawade@princeton.edu>
2019-04-10 20:20:17 -0400

    Fixed util, signal, and Window for python 3.7 from pytomo3d

---
+++ gcmt3d/window/tests/test_window.py
@@ -0,0 +1,398 @@
+import os
+import inspect
+import pytest
+import json
+
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+from obspy import read, read_inventory, read_events, UTCDateTime
+from pyflex import WindowSelector
+from pyflex.window import Window
+import pytomo3d.window.window as win
+import pytomo3d.window.io as wio
+import warnings
+
+
+def assertUTCDateTimeEqual(UTC1, UTC2):
+    """Testing whether UTC Date and time are equal."""
+    assert UTC1 == UTC2
+
+
+def is_numeric(obj):
+    attrs = ['__add__', '__sub__', '__mul__', '__truediv__', '__pow__']
+    return all(hasattr(obj, attr) for attr in attrs)
+
+
+def assertDictAlmostEqual(dict1, dict2):
+    """Test whether two dictionaries are almost equal"""
+
+    if len(dict2) is not len(dict1):
+        raise AssertionError("Dictionaries have different sizes.")
+
+    # Go through all keys in the dictionary
+    for key in dict1:
+
+        # Get Value from dictionary
+        val1 = dict1[key]
+
+        try:
+            val2 = dict2[key]
+        except KeyError:
+            warnings.warn("One of the dictionaries did not contain a keyword.")
+            val2 = None
+
+        if val2 is None:
+            continue
+
+        elif type(val1) is dict and type(val2) is dict:
+            assertDictAlmostEqual(val1, val2)
+
+        # Check if almost equal of both values
+        elif is_numeric(val1) and is_numeric(val2):
+            np.testing.assert_almost_equal(val1, val2)
+
+        elif type(val1) is UTCDateTime and type(val2) is UTCDateTime:
+            assertUTCDateTimeEqual(val1, val2)
+
+        elif type(val1) is str and type(val2) is str:
+            assert val1 == val2
+
+        elif type(val1) is not type(val2):
+            if is_numeric(val1) and is_numeric(val2):
+                np.testing.assert_almost_equal(val1, val2)
+            else:
+                raise AssertionError("Too fields are not equal.")
+
+        elif val1 is val2 is None:
+            pass
+
+        else:
+            raise AssertionError("Dictionary: Dont know whats going on.")
+
+
+def assertListAlmostEqual(list1, list2):
+    """Test whether two lists are almost equal"""
+
+    if len(list2) is not len(list1):
+        raise AssertionError("lists have different sizes.")
+
+    # Go through all things in list
+
+    for thing1, thing2 in zip(list1, list2):
+
+        # The below things are basically just to satisfy the possible
+        # list entries
+        if type(thing1) is dict and type(thing2) is dict:
+            assertDictAlmostEqual(thing1, thing2)
+
+        # Check if almost equal of both values
+        elif type(thing1) in [int, float, complex, np.float, np.float32] \
+                and type(thing2) in \
+                [int, float, complex, np.float, np.float32]:
+            np.testing.assert_almost_equal(thing1, thing2)
+
+        elif type(thing1) is list and type(thing2) is list:
+            assertListAlmostEqual(thing1, thing2)
+
+        elif type(thing1) is str and type(thing2) is str:
+            assert thing1 == thing2
+
+        elif type(thing1) is UTCDateTime and type(thing2) is UTCDateTime:
+            assertUTCDateTimeEqual(thing1, thing2)
+
+        elif type(thing1) is not type(thing2):
+            if is_numeric(thing1) and is_numeric(thing2):
+                np.testing.assert_almost_equal(thing1, thing2)
+            else:
+                raise AssertionError("Too fields are not equal.")
+
+        elif thing1 is thing2 is None:
+            pass
+
+        else:
+            raise AssertionError("List: Dont know whats going on")
+
+
+def assertWinAlmostEqual(win1, win2, parameters):
+    """Test whether two windows are almost equal"""
+
+    # Go through all keys in the Window
+    for parameter in parameters:
+
+        # Get Value from Window 1
+        try:
+            val1 = getattr(win1, parameter)
+        except KeyError:
+            raise AssertionError('Missing field')
+
+        # Get Value from Window 2
+        try:
+            val2 = getattr(win2, parameter)
+        except KeyError:
+            raise AssertionError('Missing field')
+
+        # The below things are basically just to satisfy the possible
+        # window
+
+        if type(val1) is dict and type(val2) is dict:
+            assertDictAlmostEqual(val1, val2)
+
+        elif type(val1) is list and type(val2) is list:
+            assertListAlmostEqual(val1, val2)
+
+        # Check if almost equal of both values
+        elif is_numeric(val1) and is_numeric(val2):
+            np.testing.assert_almost_equal(val1, val2)
+
+        elif type(val1) is UTCDateTime and type(val2) is UTCDateTime:
+            assertUTCDateTimeEqual(val1, val2)
+
+        elif type(val1) is str and type(val2) is str:
+            assert val1 == val2
+
+        elif type(val1) is not type(val2):
+            if is_numeric(val1) and is_numeric(val2):
+                np.testing.assert_almost_equal(val1, val2)
+            else:
+                raise AssertionError("Too fields are not equal.")
+
+        elif val1 is val2 is None:
+            pass
+
+        else:
+            raise AssertionError("Window Dont know whats going on")
+
+
+def _upper_level(path, nlevel=4):
+    """
+    Go the nlevel dir up
+    """
+    for i in range(nlevel):
+        path = os.path.dirname(path)
+    return path
+
+
+def reset_matplotlib():
+    """
+    Reset matplotlib to a common default.
+    """
+    # Set all default values.
+    mpl.rcdefaults()
+    # Force agg backend.
+    plt.switch_backend('agg')
+    # These settings must be hardcoded for running the comparision tests and
+    # are not necessarily the default values.
+    mpl.rcParams['font.family'] = 'Bitstream Vera Sans'
+    mpl.rcParams['text.hinting'] = False
+    # Not available for all matplotlib versions.
+    try:
+        mpl.rcParams['text.hinting_factor'] = 8
+    except KeyError:
+        pass
+    import locale
+    locale.setlocale(locale.LC_ALL, str('en_US.UTF-8'))
+
+
+# Most generic way to get the data folder path.
+TESTBASE_DIR = _upper_level(
+    os.path.abspath(inspect.getfile(inspect.currentframe())), 4)
+DATA_DIR = os.path.join(TESTBASE_DIR, "tests", "data")
+
+obsfile = os.path.join(DATA_DIR, "proc", "IU.KBL.obs.proc.mseed")
+synfile = os.path.join(DATA_DIR, "proc", "IU.KBL.syn.proc.mseed")
+staxml = os.path.join(DATA_DIR, "stationxml", "IU.KBL.xml")
+quakeml = os.path.join(DATA_DIR, "quakeml", "C201009031635A.xml")
+
+
+def test_update_user_levels():
+    obs_tr = read(obsfile)[0]
+    syn_tr = read(synfile)[0]
+
+    config_file = os.path.join(DATA_DIR, "window", "27_60.BHZ.config.yaml")
+    config = wio.load_window_config_yaml(config_file)
+
+    cat = read_events(quakeml)
+    inv = read_inventory(staxml)
+
+    user_module = "pytomo3d.window.tests.user_module_example"
+    config = win.update_user_levels(user_module, config, inv, cat,
+                                    obs_tr, syn_tr)
+
+    npts = obs_tr.stats.npts
+    assert isinstance(config.stalta_waterlevel, np.ndarray)
+    assert len(config.stalta_waterlevel) == npts
+    assert isinstance(config.tshift_acceptance_level, np.ndarray)
+    assert len(config.tshift_acceptance_level) == npts
+    assert isinstance(config.dlna_acceptance_level, np.ndarray)
+    assert len(config.dlna_acceptance_level) == npts
+    assert isinstance(config.cc_acceptance_level, np.ndarray)
+    assert len(config.cc_acceptance_level) == npts
+    assert isinstance(config.s2n_limit, np.ndarray)
+    assert len(config.s2n_limit) == npts
+
+
+def test_update_user_levels_raise():
+    user_module = "pytomo3d.window.tests.which_does_not_make_sense"
+    with pytest.raises(Exception) as errmsg:
+        win.update_user_levels(user_module, None, None, None,
+                               None, None)
+
+    assert "Could not import the user_function module" in str(errmsg)
+
+    user_module = "pytomo3d.window.io"
+    with pytest.raises(Exception) as errmsg:
+        win.update_user_levels(user_module, None, None, None,
+                               None, None)
+    assert "Given user module does not have a generate_user_levels method" \
+        in str(errmsg)
+
+
+def test_window_on_trace():
+    obs_tr = read(obsfile).select(channel="*R")[0]
+    syn_tr = read(synfile).select(channel="*R")[0]
+
+    config_file = os.path.join(DATA_DIR, "window", "27_60.BHZ.config.yaml")
+    config = wio.load_window_config_yaml(config_file)
+
+    cat = read_events(quakeml)
+    inv = read_inventory(staxml)
+
+    windows = win.window_on_trace(obs_tr, syn_tr, config, station=inv,
+                                  event=cat, _verbose=False,
+                                  figure_mode=False)
+
+    assert len(windows) == 5
+
+    winfile_bm = os.path.join(DATA_DIR, "window",
+                              "IU.KBL..BHR.window.json")
+
+    parameters = ["left", "right", "center", "time_of_first_sample",
+                  "max_cc_value", "cc_shift", "dlnA", "dt", "min_period",
+                  "channel_id", "phase_arrivals", "weight_function"]
+
+    with open(winfile_bm) as fh:
+        windows_json = json.load(fh)
+        for _win, _win_json_bm in zip(windows, windows_json):
+            _win_bm = Window._load_from_json_content(_win_json_bm)
+
+        assertWinAlmostEqual(_win, _win_bm, parameters)
+
+
+def test_window_on_trace_user_levels():
+    obs_tr = read(obsfile)[0]
+    syn_tr = read(synfile)[0]
+
+    config_file = os.path.join(DATA_DIR, "window", "27_60.BHZ.config.yaml")
+    config = wio.load_window_config_yaml(config_file)
+
+    cat = read_events(quakeml)
+    inv = read_inventory(staxml)
+    user_module = "pytomo3d.window.tests.user_module_example"
+
+    windows = win.window_on_trace(obs_tr, syn_tr, config, station=inv,
+                                  event=cat, user_module=user_module,
+                                  _verbose=False,
+                                  figure_mode=False)
+    assert len(windows) == 4
+
+
+def test_window_on_trace_with_none_user_levels():
+    obs_tr = read(obsfile).select(channel="*R")[0]
+    syn_tr = read(synfile).select(channel="*R")[0]
+
+    config_file = os.path.join(DATA_DIR, "window", "27_60.BHZ.config.yaml")
+    config = wio.load_window_config_yaml(config_file)
+
+    cat = read_events(quakeml)
+    inv = read_inventory(staxml)
+
+    windows = win.window_on_trace(obs_tr, syn_tr, config, station=inv,
+                                  event=cat, user_module="None",
+                                  _verbose=False, figure_mode=False)
+
+    winfile_bm = os.path.join(DATA_DIR, "window",
+                              "IU.KBL..BHR.window.json")
+
+    parameters = ["left", "right", "center", "time_of_first_sample",
+                  "max_cc_value", "cc_shift", "dlnA", "dt", "min_period",
+                  "channel_id", "phase_arrivals", "weight_function"]
+
+    with open(winfile_bm) as fh:
+        windows_json = json.load(fh)
+    for _win, _win_json_bm in zip(windows, windows_json):
+        _win_bm = Window._load_from_json_content(_win_json_bm)
+
+        assertWinAlmostEqual(_win, _win_bm, parameters)
+
+
+def test_window_on_stream():
+    obs_tr = read(obsfile)
+    syn_tr = read(synfile)
+
+    config_file = os.path.join(DATA_DIR, "window", "27_60.BHZ.config.yaml")
+    config = wio.load_window_config_yaml(config_file)
+    config_dict = {"Z": config, "R": config, "T": config}
+
+    config_file = os.path.join(DATA_DIR, "window", "27_60.BHZ.config.yaml")
+    config = wio.load_window_config_yaml(config_file)
+
+    cat = read_events(quakeml)
+    inv = read_inventory(staxml)
+
+    windows = win.window_on_stream(obs_tr, syn_tr, config_dict, station=inv,
+                                   event=cat, _verbose=False,
+                                   figure_mode=False)
+
+    assert len(windows) == 3
+    nwins = dict((_w, len(windows[_w])) for _w in windows)
+    assert nwins == {"IU.KBL..BHR": 5, "IU.KBL..BHZ": 2, "IU.KBL..BHT": 4}
+
+
+def test_window_on_stream_user_levels():
+    obs_tr = read(obsfile)
+    syn_tr = read(synfile)
+
+    config_file = os.path.join(DATA_DIR, "window", "27_60.BHZ.config.yaml")
+    config = wio.load_window_config_yaml(config_file)
+    config_dict = {"Z": config, "R": config, "T": config}
+
+    config_file = os.path.join(DATA_DIR, "window", "27_60.BHZ.config.yaml")
+    config = wio.load_window_config_yaml(config_file)
+
+    cat = read_events(quakeml)
+    inv = read_inventory(staxml)
+
+    _mod = "pytomo3d.window.tests.user_module_example"
+    user_modules = {"BHZ": _mod, "BHR": _mod, "BHT": _mod}
+
+    windows = win.window_on_stream(obs_tr, syn_tr, config_dict, station=inv,
+                                   event=cat, user_modules=user_modules,
+                                   _verbose=False,
+                                   figure_mode=False)
+
+    assert len(windows) == 3
+    nwins = dict((_w, len(windows[_w])) for _w in windows)
+    assert nwins == {"IU.KBL..BHR": 5, "IU.KBL..BHZ": 2, "IU.KBL..BHT": 4}
+
+
+def test_plot_window_figure(tmpdir):
+    reset_matplotlib()
+
+    obs_tr = read(obsfile).select(channel="*R")[0]
+    syn_tr = read(synfile).select(channel="*R")[0]
+
+    config_file = os.path.join(DATA_DIR, "window", "27_60.BHZ.config.yaml")
+    config = wio.load_window_config_yaml(config_file)
+
+    cat = read_events(quakeml)
+    inv = read_inventory(staxml)
+
+    ws = WindowSelector(obs_tr, syn_tr, config, event=cat, station=inv)
+    windows = ws.select_windows()
+
+    assert len(windows) > 0
+
+    win.plot_window_figure(str(tmpdir), obs_tr.id, ws, True,
+                           figure_format="png")
---
+++ gcmt3d/window/window.py
@@ -0,0 +1,257 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Methods that handles Window selection
+
+:copyright:
+    Wenjie Lei (lei@princeton.edu), 2016
+:license:
+    GNU Lesser General Public License, version 3 (LGPLv3)
+    (http://www.gnu.org/licenses/lgpl-3.0.en.html)
+"""
+import os
+import pyflex
+import obspy
+import copy
+import importlib
+
+
+def plot_window_figure(figure_dir, figure_id, ws, _verbose=False,
+                       figure_format="pdf"):
+    """
+    Plot window figure out
+
+    :param figure_dir: output figure directory
+    :type figure_dir: str
+    :param figure_id: figure id to distinguish windows plots, for
+        example, trace id could be used, like "II.AAK.00.BHZ"
+    :type figure_id: str
+    :param ws: window selector object from pyflex
+    :type ws: pyflex.WindowSelector
+    :param _verbose: verbose output flag
+    :type _verbose: bool
+    :param figure_format: figure format, could be "pdf", "png" and etc.
+    :type figure_format: str
+    :return:
+    """
+    outfn = "%s.%s" % (figure_id, figure_format)
+    figfn = os.path.join(figure_dir, outfn)
+    if _verbose:
+        print("Output window figure:", figfn)
+    ws.plot(figfn)
+
+
+def update_user_levels(user_module, config, station, event, obsd, synt):
+    """Update user levels as an array using user_module
+
+    This function emulates the user_functions feature in FLEXWIN. User
+    has to prepare a python module that has a method called
+    generate_user_levels. This module has to be passed out as a string
+    (It should be formatted as you would in an import statement). And
+    then, user_module is dynamically imported and the
+    generate_user_levels function is called with the following
+    parameters: pyflex config, obspy station data, obspy event data,
+    observed and synthetic obspy traces. It is excepted that user to
+    use given scalar acceptance levels as base values and generate
+    acceptance level arrays as long as trace data to be given to the
+    pyflex. Function should return these values in order: stalta water
+    level, tshift acceptance level, dlna acceptance level,
+    cc_acceptance_level, s2n_limit.
+
+    :param user_module: user module as a string
+    :type user_module: str
+    :param config: window selection config
+    :type config_dict: pyflex.Config
+    :param station: station information which provids station location to
+        calculate the epicenter distance
+    :type station: obspy.Inventory or pyflex.Station
+    :param event: event information, providing the event information
+    :type event: pyflex.Event, obspy.Catalog or obspy.Event
+    :param observed: observed trace
+    :type observed: obspy.Trace
+    :param synthetic: synthetic trace
+    :type synthetic: obspy.Trace
+    :return: window selection config with arrays
+    :rtype: pyflex.Config
+    """
+    # Ridvan Orsvuran, 2016
+    # If user gives a user_module, allow it to create the user
+    # acceptance levels as arrays.
+    try:
+        # Add current working directory to path. This enables user to
+        # use python files in the working directory.
+        # import sys
+        # sys.path.append(".")
+
+        # Import the user module
+        user = importlib.import_module(user_module)
+        # Assign generate function to a variable. This enables to
+        # catch the AttributeError.
+        generate_user_levels = user.generate_user_levels
+    except ImportError:
+        raise Exception("Could not import the user_function module: %s"
+                        % user_module)
+    except AttributeError:
+        raise Exception("Given user module does not have a "
+                        "generate_user_levels method: %s" % user_module)
+
+    # do not give the original config to the user
+    config_copy = copy.deepcopy(config)
+    stalta_waterlevel, tshift, dlna, cc, s2n = generate_user_levels(
+        config_copy, station, event, obsd, synt)
+
+    # Create a new config using new acceptance levels
+    new_config = copy.deepcopy(config)
+    new_config.stalta_waterlevel = stalta_waterlevel
+    new_config.tshift_acceptance_level = tshift
+    new_config.dlna_acceptance_level = dlna
+    new_config.cc_acceptance_level = cc
+    new_config.s2n_limit = s2n
+
+    return new_config
+
+
+def window_on_trace(obs_tr, syn_tr, config, station=None,
+                    event=None, user_module=None, _verbose=False,
+                    figure_mode=False, figure_dir=None,
+                    figure_format="pdf"):
+    """
+    Window selection on a trace(obspy.Trace)
+
+    :param observed: observed trace
+    :type observed: obspy.Trace
+    :param synthetic: synthetic trace
+    :type synthetic: obspy.Trace
+    :param config: window selection config
+    :type config_dict: pyflex.Config
+    :param station: station information which provids station location to
+        calculate the epicenter distance
+    :type station: obspy.Inventory or pyflex.Station
+    :param event: event information, providing the event information
+    :type event: pyflex.Event, obspy.Catalog or obspy.Event
+    :param user_module: user module as a string
+    :type user_module: str
+    :param figure_mode: output figure flag
+    :type figure_mode: bool
+    :param figure_dir: output figure directory
+    :type figure_dir: str
+    :param _verbose: verbose flag
+    :type _verbose: bool
+    :return:
+    """
+
+    if not isinstance(obs_tr, obspy.Trace):
+        raise ValueError("Input obs_tr should be obspy.Trace")
+    if not isinstance(syn_tr, obspy.Trace):
+        raise ValueError("Input syn_tr should be obspy.Trace")
+    if not isinstance(config, pyflex.Config):
+        raise ValueError("Input config should be pyflex.Config")
+
+    # Ridvan Orsvuran, 2016
+    # If user gives a user_module, use it to update acceptance levels
+    # as arrays.
+    if user_module is not None and user_module != "None":
+        config = update_user_levels(user_module, config, station,
+                                    event, obs_tr, syn_tr)
+
+    ws = pyflex.WindowSelector(obs_tr, syn_tr, config,
+                               event=event, station=station)
+    try:
+        windows = ws.select_windows()
+    except Exception as err:
+        print("Error(%s): %s" % (obs_tr.id, err))
+        windows = []
+
+    if figure_mode:
+        plot_window_figure(figure_dir, obs_tr.id, ws, _verbose,
+                           figure_format=figure_format)
+
+    if _verbose:
+        print("Station %s picked %i windows" % (obs_tr.id, len(windows)))
+
+    return windows
+
+
+def window_on_stream(observed, synthetic, config_dict, station=None,
+                     event=None, user_modules=None,
+                     figure_mode=False, figure_dir=None,
+                     _verbose=False):
+    """
+    Window selection on a Stream
+
+    :param observed: observed stream
+    :type observed: obspy.Stream
+    :param synthetic: synthetic stream
+    :type synthetic: obspy.Stream
+    :param config_dict: window selection config dictionary, for example,
+        {"Z": pyflex.Config, "R": pyflex.Config, "T": pyflex.Config}
+    :type config_dict: dict
+    :param station: station information which provids station location to
+        calculate the epicenter distance
+    :type station: obspy.Inventory or pyflex.Station
+    :param event: event information, providing the event information
+    :type event: pyflex.Event, obspy.Catalog or obspy.Event
+    :param user_modules: user_module strings in a dict similar to config_dict.
+    :type user_modules: dict
+    :param figure_mode: output figure flag
+    :type figure_mode: bool
+    :param figure_dir: output figure directory
+    :type figure_dir: str
+    :param _verbose: verbose flag
+    :type _verbose: bool
+    :return:
+    """
+    if not isinstance(observed, obspy.Stream):
+        raise ValueError("Input observed should be obspy.Stream")
+    if not isinstance(synthetic, obspy.Stream):
+        raise ValueError("Input synthetic should be obspy.Stream")
+    if not isinstance(config_dict, dict):
+        raise ValueError("Input config_dict should be dict")
+
+    all_windows = {}
+
+    # Ridvan Orsvuran, 2016
+    # Assign an empty dict to user_modules if it is None to avoid errors.
+    if user_modules is None:
+        user_modules = {}
+
+    for category in config_dict:
+        config_base = config_dict[category]
+        user_module = user_modules.get(category, None)
+        if len(category) == 1:
+            # then it is component
+            obs = observed.select(component=category)
+        elif len(category) == 3:
+            # then it is channel
+            obs = observed.select(channel=category)
+        else:
+            raise ValueError("The length of Config_dict.keys()[%s] should be "
+                             "either 1 or 3, for example, ['E', 'N', 'Z'] "
+                             "or ['BHE', 'BHN', 'BHZ']" % config_dict.keys())
+
+        for obs_tr in obs:
+            component = obs_tr.stats.channel[-1]
+            try:
+                syn_tr = synthetic.select(station=obs_tr.stats.station,
+                                          network=obs_tr.stats.network,
+                                          component=component)[0]
+            except Exception as err:
+                print("Couldn't find corresponding synt for obsd trace(%s):"
+                      "%s" % (obs_tr.id, err))
+                continue
+
+            config = copy.deepcopy(config_base)
+            windows = window_on_trace(
+                obs_tr, syn_tr, config, station=station,
+                event=event, user_module=user_module, _verbose=_verbose,
+                figure_mode=figure_mode, figure_dir=figure_dir)
+
+            if windows is None:
+                continue
+
+            # Notice: Ebru suggests to write out window even its length is
+            # zero, which means no windows selected on the traces, in order
+            # to keep track of every thing
+            all_windows[obs_tr.id] = windows
+
+    return all_windows
