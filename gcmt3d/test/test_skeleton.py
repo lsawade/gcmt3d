9c475fadf0ec539ae15bf780b95b37bd9e49e7be
Lucas Sawade <lsawade@princeton.edu>
2019-05-02 12:00:40 -0400

    All tests including sanity checks pass

--- gcmt3d/test/test_skeleton.py
+++ gcmt3d/test/test_skeleton.py
@@ -12,13 +12,28 @@ Last Update: April 2019
 
 """
 
-
-
 import unittest
 from gcmt3d.data.management.skeleton import DataBaseSkeleton
 from gcmt3d.source import CMTSource
 import tempfile
 import os
+import inspect
+
+
+def _upper_level(path, nlevel=4):
+    """
+    Go the nlevel dir up
+    """
+    for i in range(nlevel):
+        path = os.path.dirname(path)
+    return path
+
+
+# Most generic way to get the data folder path.
+TESTBASE_DIR = _upper_level(os.path.abspath(
+    inspect.getfile(inspect.currentframe())), 1)
+DATA_DIR = os.path.join(TESTBASE_DIR, "data")
+
 
 class TestSkeleton(unittest.TestCase):
     """Class that handles testing of the Data Base structure creator"""
@@ -32,12 +47,9 @@ class TestSkeleton(unittest.TestCase):
         os.makedirs(os.path.join(self.specfem_dir, "OUTPUT_FILES"))
         os.makedirs(os.path.join(self.specfem_dir, "bin"))
 
-
-
     def tearDown(self):
         # Close the file, the directory will be removed after the test
         self.test_dir.cleanup()
-
 
     def test_IO(self):
         """Testing if strings are set correctly."""
@@ -45,11 +57,11 @@ class TestSkeleton(unittest.TestCase):
         with tempfile.TemporaryDirectory() as tmp_dir:
             # Initialize database skeleton class
             DB = DataBaseSkeleton(basedir=tmp_dir,
-                                  cmt_fn="./data/CMTSOLUTION",
+                                  cmt_fn=os.path.join(DATA_DIR, "CMTSOLUTION"),
                                   specfem_dir=self.specfem_dir, verbose=False)
 
             self.assertEqual(DB.specfem_dir, self.specfem_dir)
-            self.assertEqual(DB.cmt_fn, "./data/CMTSOLUTION")
+            self.assertEqual(DB.cmt_fn, os.path.join(DATA_DIR, "CMTSOLUTION"))
             self.assertEqual(DB.basedir, tmp_dir)
 
     def test_create_db_dir(self):
@@ -60,7 +72,7 @@ class TestSkeleton(unittest.TestCase):
 
             # Initialize database skeleton class
             DB = DataBaseSkeleton(basedir=tmp_dir,
-                                  cmt_fn="./data/CMTSOLUTION",
+                                  cmt_fn=os.path.join(DATA_DIR, "CMTSOLUTION"),
                                   specfem_dir=self.specfem_dir,
                                   verbose=True)
 
@@ -72,7 +84,7 @@ class TestSkeleton(unittest.TestCase):
         with tempfile.TemporaryDirectory() as tmp_dir:
             # Initialize database skeleton class
             DB = DataBaseSkeleton(basedir=os.path.join(tmp_dir, "db"),
-                                  cmt_fn="./data/CMTSOLUTION",
+                                  cmt_fn=os.path.join(DATA_DIR, "CMTSOLUTION"),
                                   specfem_dir=self.specfem_dir,
                                   verbose=True)
 
@@ -88,7 +100,7 @@ class TestSkeleton(unittest.TestCase):
         with tempfile.TemporaryDirectory() as tmp_dir:
 
             # Cmtfile path
-            cmtfile = "./data/CMTSOLUTION"
+            cmtfile = os.path.join(DATA_DIR, "CMTSOLUTION")
             # create CMT
             cmt = CMTSource.from_CMTSOLUTION_file(cmtfile)
 
@@ -111,8 +123,8 @@ class TestSkeleton(unittest.TestCase):
             new_cmt_path = os.path.join(eq_dir, "eq_" + eq_id + ".cmt")
             self.assertTrue(os.path.exists(new_cmt_path) and os.path.isfile(
                 new_cmt_path))
-            self.assertTrue(CMTSource.from_CMTSOLUTION_file(new_cmt_path) ==
-                            cmt)
+            self.assertTrue(CMTSource.from_CMTSOLUTION_file(new_cmt_path)
+                            == cmt)
 
     def test_create_eq_dir_mult(self):
         """Tests creation of earthquake directory and copying of the cmt
@@ -122,8 +134,8 @@ class TestSkeleton(unittest.TestCase):
         with tempfile.TemporaryDirectory() as tmp_dir:
 
             # Cmtfile path
-            cmtfile1 = "./data/CMTSOLUTION_TRUE"
-            cmtfile2 = "./data/CMTSOLUTION_VAR"
+            cmtfile1 = os.path.join(DATA_DIR, "CMTSOLUTION_TRUE")
+            cmtfile2 = os.path.join(DATA_DIR, "CMTSOLUTION_VAR")
 
             # create CMT
             cmt1 = CMTSource.from_CMTSOLUTION_file(cmtfile1)
@@ -139,7 +151,8 @@ class TestSkeleton(unittest.TestCase):
 
             # Initialize database skeleton class
             DB = DataBaseSkeleton(basedir=tmp_dir,
-                                  cmt_fn="./data/CMTSOLUTION_*",
+                                  cmt_fn=os.path.join(DATA_DIR,
+                                                      "CMTSOLUTION_*"),
                                   specfem_dir=self.specfem_dir,
                                   verbose=True)
 
@@ -160,7 +173,7 @@ class TestSkeleton(unittest.TestCase):
         # Check one cmt file
         with tempfile.TemporaryDirectory() as tmp_dir:
             # Cmtfile path
-            cmtfile = "./data/CMTSOLUTION"
+            cmtfile = os.path.join(DATA_DIR, "CMTSOLUTION")
 
             # Initialize database skeleton class
             DB = DataBaseSkeleton(basedir=tmp_dir,
@@ -180,7 +193,7 @@ class TestSkeleton(unittest.TestCase):
         # Check one cmt file
         with tempfile.TemporaryDirectory() as tmp_dir:
             # Cmtfile path
-            cmtfile = "./data/CMTSOLUTION"
+            cmtfile = os.path.join(DATA_DIR, "CMTSOLUTION")
 
             # Initialize database skeleton class
             DB = DataBaseSkeleton(basedir=tmp_dir,
@@ -195,9 +208,8 @@ class TestSkeleton(unittest.TestCase):
             self.assertTrue(os.path.exists(new_cmt_path)
                             and os.path.isfile(new_cmt_path))
 
-            self.assertTrue(CMTSource.from_CMTSOLUTION_file(new_cmt_path) ==
-                            CMTSource.from_CMTSOLUTION_file(cmtfile))
-
+            self.assertTrue(CMTSource.from_CMTSOLUTION_file(new_cmt_path)
+                            == CMTSource.from_CMTSOLUTION_file(cmtfile))
 
     def test_create_SIM_dir(self):
         """Tests the function that creates the Simulation directories and the
@@ -206,7 +218,7 @@ class TestSkeleton(unittest.TestCase):
         with tempfile.TemporaryDirectory() as tmp_dir:
 
             # Cmtfile path
-            cmtfile = "./data/CMTSOLUTION"
+            cmtfile = os.path.join(DATA_DIR, "CMTSOLUTION")
 
             # create CMT
             cmt = CMTSource.from_CMTSOLUTION_file(cmtfile)
@@ -253,9 +265,6 @@ class TestSkeleton(unittest.TestCase):
                 # Check if link is created
                 self.assertTrue(os.path.islink(os.path.join(test_dir, "bin")))
 
-
-
-
     def test__copy_dir(self):
         """Tests the copy dir function in skeleton."""
 
@@ -267,7 +276,7 @@ class TestSkeleton(unittest.TestCase):
             os.makedirs(test_dir1)
 
             # Cmtfile path
-            cmtfile = "./data/CMTSOLUTION"
+            cmtfile = os.path.join(DATA_DIR, "CMTSOLUTION")
 
             # Initialize database skeleton class
             DB = DataBaseSkeleton(basedir=tmp_dir,
@@ -280,7 +289,5 @@ class TestSkeleton(unittest.TestCase):
             self.assertTrue(os.path.isdir(test_dir2))
 
 
-
 if __name__ == "__main__":
     unittest.main()
-
