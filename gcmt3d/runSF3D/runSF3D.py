481473c248d8cddc04cb417aa26e3f802405b850
Lucas Sawade <lsawade@princeton.edu>
2019-05-20 17:42:40 -0400

    going to test the specfem run thing

--- gcmt3d/runSF3D/runSF3D.py
+++ gcmt3d/runSF3D/runSF3D.py
@@ -11,13 +11,15 @@ Last Update: May 2019
 
 
 """
-
+import os
+import subprocess
+from warnings import warn
 
 class RunSimulation(object):
     """Class handles the running of specfem after its directory has been
     added to the database. """
 
-    def __init__(self, database_dir, npar=9, n=1):
+    def __init__(self, earthquake_dir, npar=9, n=1, verbose=False):
         """
         Initializes Run parameters
 
@@ -25,20 +27,53 @@ class RunSimulation(object):
             database_eq_dir: string with directory name
             N: integer with number of Nodes
             n: integer with number of tasks
-            npar: integer number of parameters .
+            npar: integer number of parameters
+            verbose: boolean deciding on whether to print stuff
 
         Returns: Nothing really it just runs specfem with the above options
 
         """
 
-
+        self.earthquake = earthquake_dir
+        self.simdir = os.path.join(self.earthquake, "CMT_SIMs")
         self.N = N
         self.n = n
         self.npar = npar
+        self.batchdir = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                     "batch")
+        self.v = verbose
 
     def __call__(self):
         """Runs the Simulation using the shell and batch files in the batch
         subdirectory."""
+
+        # batch driver script
+        batchscript = os.path.join(self.batchdir, "drive.sbatch")
+
+        bashCommand = "drive.sh %s %s %s %s %s" % (self.N, self.n,
+                                                   self.npar, self.simdir,
+                                                   batchscript)
+
+        # Start process
+        process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+
+        # catch outputs
+        output, error = process.communicate()
+
+        if self.v:
+            print("Output:\n", output)
+            warn("Errors:\n", error)
+
+    def __str__(self):
+        """string return"""
+
+        string = ""
+        string += "Earthquake directory: %d\n" % self.earthquake
+        string += "Simulation directory: %d\n" % self.simdir
+        string += "Number of Nodes: %d\n" % self.N
+        string += "Number of Tasks: %d\n" % self.n
+        string += "Number of Parameters: %d\n\n" % self.npar
+
 
 
 
