4b11ff38cb64adf93b9741c0ac50c9d7686dfb5d
Lucas Sawade <lsawade@princeton.com>
2019-05-23 13:39:28 -0400

    Changed a few directories

--- gcmt3d/runSF3D/batch/drive.sh
+++ gcmt3d/runSF3D/batch/drive.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Get command line arguments
-NODES=$1
-TASKS=$2
-NPAR=0-$(($3-1))
-RUNDIR=$4
-SCRIPT=$5
-VERBOSE=$6
+NODES="$1"
+TASKS="$2"
+NPAR="0-$(($3-1))"
+RUNDIR="$4"
+SCRIPT="$5"
+VERBOSE="$6"
 
 if [ "$VERBOSE" -eq "1" ]
     then
@@ -25,9 +25,16 @@ if [ "$VERBOSE" -eq "1" ]
 fi
 
 # Run batch script
-sbatch -N $NODES -n $TASKS --array=$NPAR -t $TIME --export=RUNDIR=$RUNDIR,NODES=$NODES,TASKS=$TASKS,NPAR=$NPAR,VERBOSE=$VERBOSE --output=$RUNDIR/job_%a.out --error=$RUNDIR/job_%a.err $SCRIPT
+echo "-N $NODES -n $TASKS --array=$NPAR -t $TIME --export=RUNDIR=$RUNDIR,NODES=$NODES,TASKS=$TASKS,NPAR=$NPAR,VERBOSE=$VERBOSE --output=$RUNDIR/job_%a.out --error=$RUNDIR/job_%a.err $SCRIPT"
+
+# sbatch -N "$NODES" -n $TASKS --array=$NPAR -t $TIME --export=RUNDIR=$RUNDIR,NODES=$NODES,TASKS=$TASKS,NPAR=$NPAR,VERBOSE=$VERBOSE --output=$RUNDIR/job_%a.out --error=$RUNDIR/job_%a.err $SCRIPT
+
+sbatch -N 1 -n 24 --array=0-8 -t 00:30:00 --export=RUNDIR=$RUNDIR,NODES=$NODES,TASKS=$TASKS,NPAR=$NPAR,VERBOSE=$VERBOSE --output=$RUNDIR/job_%a.out --error=$RUNDIR/job_%a.err $SCRIPT
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cat $DIR/drive.sbatch
 
 if [ "$VERBOSE" -eq "1" ]
     then
       echo "SUBMITTED."
+
 fi
