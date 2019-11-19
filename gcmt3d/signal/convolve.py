e96c1decd5e15c33e0f48a3e58ce20da031cd715
Lucas Sawade <lsawade@princeton.edu>
2019-11-19 17:03:06 -0500

    Added and fixed some slurm stuff

---
+++ gcmt3d/signal/convolve.py
@@ -0,0 +1,54 @@
+"""
+
+This script contains functions to convolve the Green's functions in a Stream
+with a source time function.
+
+:copyright:
+    Lucas Sawade (lsawade@princeton.edu)
+:license:
+    GNU General Public License, Version 3
+    (http://www.gnu.org/copyleft/gpl.html)
+
+Last Update: November 2019
+
+"""
+
+def gaussian
+
+class Wavelet(object):
+    """Contains wavelet"""
+
+    def __init__(self, t, data=None, type='gaussian', **kwargs):
+        """ Creates wavelet with parameters
+
+        :param dt: sampling time
+        :param N:
+        :param type: type of automatically created wavelet. Irrelevant if
+                     data is input.
+        :param
+        :param **kwargs: keyword arguments depending on the type of wavelet
+
+        """
+
+
+
+    @classmethod
+    def wavelet_from_SAC(cls, filename):
+        """ Creates
+
+        :param filename: filename to create wavelet from
+        :type: str
+        :return:
+
+        """
+        pass
+
+        # return cls()
+
+
+
+
+def convolve_stream(st, wavelet):
+    """Takes in a wavelet"""
+
+    pass
