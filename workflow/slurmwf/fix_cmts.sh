375021b5e1e1d903c7b81de12585afd634025cc0
Lucas Sawade <lsawade@princeton.edu>
2020-04-08 11:49:09 -0400

    fix_cmts.sh

--- workflow/slurmwf/fix_cmts.sh
+++ workflow/slurmwf/fix_cmts.sh
@@ -10,7 +10,7 @@ do
     # This line does magic. 
     # it replace the line that starts with event name with a new line 
     # that takes the value from the file and strips the C from the id
-    sed -i 's/^event name: .*$/event name:    '$(cat $CMTDIR/$file | head -2 | tail -1 | cut -d: -f2 | tr -d '[:space:]' | tr -d "C")'/' $CMTDIR/$file
+    sed -i 's/^event name: .*$/event name:    '$(cat $CMTDIR/$file | head -2 | tail -1 | cut -d: -f2 | tr -d '[:space:]' | sed 's/^.\{1\}//')'/' $CMTDIR/$file
 
 done
 
