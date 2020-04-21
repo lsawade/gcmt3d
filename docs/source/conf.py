fc1eb8f01ec4fe17b263eb013887e448a1d773af
Lucas Sawade <lsawade@princeton.edu>
2020-04-20 22:54:24 -0400

    changed source docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,11 +10,6 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
-import sys
-sys.path.insert(0, os.path.abspath('../workflow/workflow'))
-sys.path.insert(0, os.path.abspath('../workflow/params'))
-sys.path.insert(0, os.path.abspath('../gcmt3d'))
 
 # -- Project information -----------------------------------------------------
 
