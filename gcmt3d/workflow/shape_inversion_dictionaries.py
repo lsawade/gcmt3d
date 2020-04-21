b240fe711584d2cbe5f07174e3bf8737c34310e7
Lucas Sawade <lsawade@princeton.edu>
2020-04-20 22:21:26 -0400

    added cosine taper

--- gcmt3d/workflow/shape_inversion_dictionaries.py
+++ gcmt3d/workflow/shape_inversion_dictionaries.py
@@ -35,13 +35,6 @@ def inversion_dictionaries(cmt_file_db, param_path):
     :return:
     """
 
-    # Get database parameter path
-    databaseparam_path = os.path.join(param_path,
-                                      "Database/DatabaseParameters.yml")
-
-    # Load Parameters
-    DB_params = read_yaml_file(databaseparam_path)
-
     # Processing param dir
     process_obs_dir = os.path.join(param_path, "ProcessObserved")
     process_syn_dir = os.path.join(param_path, "ProcessSynthetic")
@@ -54,8 +47,7 @@ def inversion_dictionaries(cmt_file_db, param_path):
         create_full_inversion_dict_list(cmt_file_db,
                                         process_obs_dir,
                                         process_syn_dir,
-                                        window_process_dir,
-                                        npar=DB_params['npar'])
+                                        window_process_dir)
 
     # Write the dictionaries
     write_inversion_dicts(inv_dict_list, filename_list)
