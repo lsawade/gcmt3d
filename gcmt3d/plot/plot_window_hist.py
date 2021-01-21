32e74df0c536352e63f9ecdc02e2568f49b5c5a5
Lucas Sawade <lsawade@princeton.edu>
2021-01-21 00:25:09 -0500

    Added exception layer to get measurements

--- gcmt3d/plot/plot_window_hist.py
+++ gcmt3d/plot/plot_window_hist.py
@@ -83,50 +83,57 @@ def load_winfile_json(flexwin_file: str, inv: Inventory, event: CMTSource,
                     max_cc[_idx] = _win["max_cc_value"]
 
                 # Compute epicentral distance
-                if 'BHR' in channel_id:
-                    channel = 'R'
-                    for chan in ['BH1', 'BHE']:
-                        try:
-                            nc = channel_id[:-3] + chan
-                            coords = inv.get_coordinates(nc, event.cmt_time)
-                            break
-                        except Exception as e:
-                            if v:
-                                print(channel_id, nc, e)
+                try:
+                    if 'BHR' in channel_id:
+                        channel = 'R'
+                        for chan in ['BH1', 'BHE']:
+                            try:
+                                nc = channel_id[:-3] + chan
+                                coords = inv.get_coordinates(
+                                    nc, event.cmt_time)
+                                break
+                            except Exception as e:
+                                if v:
+                                    print(channel_id, nc, e)
 
-                elif 'BHT' in channel_id:
-                    channel = 'T'
-                    for chan in ['BH2', 'BHN']:
-                        try:
-                            nc = channel_id[:-3] + chan
-                            coords = inv.get_coordinates(nc, event.cmt_time)
-                            break
-                        except Exception as e:
-                            if v:
-                                print(channel_id, nc, e)
-                else:
-                    channel = 'Z'
-                    coords = inv.get_coordinates(channel_id, event.cmt_time)
+                    elif 'BHT' in channel_id:
+                        channel = 'T'
+                        for chan in ['BH2', 'BHN']:
+                            try:
+                                nc = channel_id[:-3] + chan
+                                coords = inv.get_coordinates(
+                                    nc, event.cmt_time)
+                                break
+                            except Exception as e:
+                                if v:
+                                    print(channel_id, nc, e)
+                    else:
+                        channel = 'Z'
+                        coords = inv.get_coordinates(
+                            channel_id, event.cmt_time)
 
-                # Compute epicentral distance
-                epi = kilometer2degrees(gps2dist_azimuth(
-                    event.latitude, event.longitude,
-                    coords["latitude"], coords["longitude"])[0]/1000.0)
+                    # Compute epicentral distance
+                    epi = kilometer2degrees(gps2dist_azimuth(
+                        event.latitude, event.longitude,
+                        coords["latitude"], coords["longitude"])[0]/1000.0)
 
-                # Summarize time values and distances
-                for _idx, (_dt, _dlnA, _cc_shift, _max_cc) in \
-                        enumerate(zip(dt, dlnA, cc_shift, max_cc)):
-                    times = np.arange(win_time[_idx, 0],
-                                      win_time[_idx, 1] + _dt, _dt).tolist()
-                    measurement_dict[channel]["wins"].extend(times)
-                    measurement_dict[channel]["epics"].extend(
-                        np.ones(len(times)) * epi)
-                    measurement_dict[channel]["dlnAs"].extend(
-                        np.ones(len(times)) * _dlnA)
-                    measurement_dict[channel]["cc_shifts"].extend(
-                        np.ones(len(times)) * _cc_shift)
-                    measurement_dict[channel]["max_ccs"].extend(
-                        np.ones(len(times)) * _max_cc)
+                    # Summarize time values and distances
+                    for _idx, (_dt, _dlnA, _cc_shift, _max_cc) in \
+                            enumerate(zip(dt, dlnA, cc_shift, max_cc)):
+                        times = np.arange(win_time[_idx, 0],
+                                          win_time[_idx, 1] + _dt, _dt).tolist()
+                        measurement_dict[channel]["wins"].extend(times)
+                        measurement_dict[channel]["epics"].extend(
+                            np.ones(len(times)) * epi)
+                        measurement_dict[channel]["dlnAs"].extend(
+                            np.ones(len(times)) * _dlnA)
+                        measurement_dict[channel]["cc_shifts"].extend(
+                            np.ones(len(times)) * _cc_shift)
+                        measurement_dict[channel]["max_ccs"].extend(
+                            np.ones(len(times)) * _max_cc)
+
+                except Exception as e:
+                    print(channel_id, e)
 
     return measurement_dict
 
